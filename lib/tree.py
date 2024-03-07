7381a442bcb759e45e9465bc30682398fda220cf
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-03-06 18:22:59 -0600

    Completed Lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,25 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      result.append(node)
+      
+      if node['id'] == id:
+        return node
+      
+      if node['children']:
+        nodes_to_visit = node['children'] + nodes_to_visit
+
+    return None
+
+
+# {
+#   'tag_name': 'h1',
+#   'id': 'heading',
+#   'text_content': 'Title',
+#   'children': []
+# }
